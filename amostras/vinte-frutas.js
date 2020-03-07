c105352f0cce10561a3b3f4c8ef1d4d45d4c4407
Fausto G. Cintra <fausto@fgcintra.pro.br>
2020-03-06 21:09:27 -0300

    (06/03) Upload das amostras de dados

---
+++ amostras/vinte-frutas.js
@@ -0,0 +1,24 @@
+let frutas = [
+   'morango',
+   'laranja',
+   'uva',
+   'framboesa',
+   'goiaba',
+   'mamão',
+   'caju',
+   'jaca',
+   'melancia',
+   'cupuaçu',
+   'acerola',
+   'abacate',
+   'cajá',
+   'manga',
+   'melão',
+   'abacaxi',
+   'tangerina',
+   'banana',
+   'limão',
+   'ameixa'
+]
+
+module.exports = frutas
